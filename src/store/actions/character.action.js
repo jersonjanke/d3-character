a499f3497cd194e77b0113822e839001cbe04a12
Jerson Janke <jersonjanke@Jersons-MacBook-Pro.local>
2021-04-30 14:04:59 -0300

    get image redux

--- src/store/actions/character.action.js
+++ src/store/actions/character.action.js
@@ -1,66 +1,128 @@
 import {
   GET_BARBARIAN,
+  GET_BARBARIAN_IMAGE,
   GET_CRUSADER,
+  GET_CRUSADER_IMAGE,
   GET_DEMON_HUNTER,
+  GET_DEMON_HUNTER_IMAGE,
   GET_MONK,
+  GET_MONK_IMAGE,
   GET_NECROMANCER,
+  GET_NECROMANCER_IMAGE,
   GET_WITCH_DOCTOR,
+  GET_WITCH_DOCTOR_IMAGE,
   GET_WIZARD,
+  GET_WIZARD_IMAGE,
 } from './type.character'
 import { getCharacterService } from '../../api'
 
+const BASE_URL = 'https://blzmedia-a.akamaihd.net/d3/icons/portraits/64'
+
+export const getCharacterImage = (character) => {
+  return `${BASE_URL}/${character}.png`
+}
+
 export const getBarbarian = () => {
   const promise = getCharacterService('barbarian')
-  return {
-    type: GET_BARBARIAN,
-    payload: promise,
-  }
+  return [
+    (dispatch) => {
+      promise.then((response) => {
+        dispatch({ type: GET_BARBARIAN, payload: response.data })
+        dispatch({
+          type: GET_BARBARIAN_IMAGE,
+          payload: getCharacterImage(response.data.icon),
+        })
+      })
+    },
+  ]
 }
 
 export const getCrusader = () => {
   const promise = getCharacterService('crusader')
-  return {
-    type: GET_CRUSADER,
-    payload: promise,
-  }
+  return [
+    (dispatch) => {
+      promise.then((response) => {
+        dispatch({ type: GET_CRUSADER, payload: response.data })
+        dispatch({
+          type: GET_CRUSADER_IMAGE,
+          payload: getCharacterImage(response.data.icon),
+        })
+      })
+    },
+  ]
 }
 
 export const getDemonHunter = () => {
   const promise = getCharacterService('demon-hunter')
-  return {
-    type: GET_DEMON_HUNTER,
-    payload: promise,
-  }
+  return [
+    (dispatch) => {
+      promise.then((response) => {
+        dispatch({ type: GET_DEMON_HUNTER, payload: response.data })
+        dispatch({
+          type: GET_DEMON_HUNTER_IMAGE,
+          payload: getCharacterImage(response.data.icon),
+        })
+      })
+    },
+  ]
 }
 
 export const getMonk = () => {
   const promise = getCharacterService('monk')
-  return {
-    type: GET_MONK,
-    payload: promise,
-  }
+  return [
+    (dispatch) => {
+      promise.then((response) => {
+        dispatch({ type: GET_MONK, payload: response.data })
+        dispatch({
+          type: GET_MONK_IMAGE,
+          payload: getCharacterImage(response.data.icon),
+        })
+      })
+    },
+  ]
 }
 
 export const getNecromancer = () => {
   const promise = getCharacterService('necromancer')
-  return {
-    type: GET_NECROMANCER,
-    payload: promise,
-  }
+  return [
+    (dispatch) => {
+      promise.then((response) => {
+        dispatch({ type: GET_NECROMANCER, payload: response.data })
+        dispatch({
+          type: GET_NECROMANCER_IMAGE,
+          payload: getCharacterImage(response.data.icon),
+        })
+      })
+    },
+  ]
 }
 
 export const getWitchDoctor = () => {
   const promise = getCharacterService('witch-doctor')
-  return {
-    type: GET_WITCH_DOCTOR,
-    payload: promise,
-  }
+  return [
+    (dispatch) => {
+      promise.then((response) => {
+        dispatch({ type: GET_WITCH_DOCTOR, payload: response.data })
+        dispatch({
+          type: GET_WITCH_DOCTOR_IMAGE,
+          payload: getCharacterImage(response.data.icon),
+        })
+      })
+    },
+  ]
 }
 
 export const getWizard = () => {
   const promise = getCharacterService('wizard')
-  return {
-    type: GET_WIZARD,
-    payload: promise,
-  }
+  return [
+    (dispatch) => {
+      promise.then((response) => {
+        dispatch({ type: GET_WIZARD, payload: response.data })
+        dispatch({
+          type: GET_WIZARD_IMAGE,
+          payload: getCharacterImage(response.data.icon),
+        })
+      })
+    },
+  ]
 }
--- src/store/reducers/character.reducer.js
+++ src/store/reducers/character.reducer.js
@@ -1,11 +1,18 @@
 import {
   GET_BARBARIAN,
+  GET_BARBARIAN_IMAGE,
   GET_CRUSADER,
+  GET_CRUSADER_IMAGE,
   GET_DEMON_HUNTER,
+  GET_DEMON_HUNTER_IMAGE,
   GET_MONK,
+  GET_MONK_IMAGE,
   GET_NECROMANCER,
+  GET_NECROMANCER_IMAGE,
   GET_WITCH_DOCTOR,
+  GET_WITCH_DOCTOR_IMAGE,
   GET_WIZARD,
+  GET_WIZARD_IMAGE,
 } from './../actions/type.character'
 const initialState = {
   barbarian: {},
@@ -16,37 +23,93 @@ const characterReducer = (state = initialState, action) => {
     case GET_BARBARIAN:
       return {
         ...state,
-        barbarian: action.payload.data,
+        barbarian: action.payload,
+      }
+    case GET_BARBARIAN_IMAGE:
+      return {
+        ...state,
+        barbarian: {
+          ...state.barbarian,
+          icon: action.payload,
+        },
       }
     case GET_CRUSADER:
       return {
         ...state,
-        crusader: action.payload.data,
+        crusader: action.payload,
+      }
+    case GET_CRUSADER_IMAGE:
+      return {
+        ...state,
+        crusader: {
+          ...state.crusader,
+          icon: action.payload,
+        },
       }
     case GET_DEMON_HUNTER:
       return {
         ...state,
-        demonHunter: action.payload.data,
+        demonHunter: action.payload,
+      }
+    case GET_DEMON_HUNTER_IMAGE:
+      return {
+        ...state,
+        demonHunter: {
+          ...state.demonHunter,
+          icon: action.payload,
+        },
       }
     case GET_MONK:
       return {
         ...state,
-        monk: action.payload.data,
+        monk: action.payload,
+      }
+    case GET_MONK_IMAGE:
+      return {
+        ...state,
+        monk: {
+          ...state.monk,
+          icon: action.payload,
+        },
       }
     case GET_NECROMANCER:
       return {
         ...state,
-        necromancer: action.payload.data,
+        necromancer: action.payload,
+      }
+    case GET_NECROMANCER_IMAGE:
+      return {
+        ...state,
+        necromancer: {
+          ...state.necromancer,
+          icon: action.payload,
+        },
       }
     case GET_WITCH_DOCTOR:
       return {
         ...state,
-        witchDoctor: action.payload.data,
+        witchDoctor: action.payload,
+      }
+    case GET_WITCH_DOCTOR_IMAGE:
+      return {
+        ...state,
+        witchDoctor: {
+          ...state.witchDoctor,
+          icon: action.payload,
+        },
       }
     case GET_WIZARD:
       return {
         ...state,
-        wizard: action.payload.data,
+        wizard: action.payload,
+      }
+    case GET_WIZARD_IMAGE:
+      return {
+        ...state,
+        wizard: {
+          ...state.wizard,
+          icon: action.payload,
+        },
       }
 
     default:
